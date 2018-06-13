96cd051a3a2e587f789c99f31f1dee468e9f975b
Guillaume Quentin <guillaumeq@eisti.eu>
2018-06-13 11:01:03 +0200

    Pioche v1.1

--- UnitPioche.pas
+++ UnitPioche.pas
@@ -8,7 +8,7 @@
  -- Edition des liens : fpc
  -- Execution         : shell
  ------------------------------------------------------------------------------------*)
- UNIT UnitPioche;
+UNIT UnitPioche;
 
  INTERFACE
  	Uses ProjUnit, Crt, SysUtils, TypInfo;
@@ -35,6 +35,28 @@ FUNCTION initPioche(pioch: pointPioche; valMax, valeur: tRegle): pointPioche;
  -- Post conditions   : Initialiser un pion de la pioche individuellement
  ------------------------------------------------------------------------------------*)
 FUNCTION initPion(pion: tPion; valeur: tRegle): tPion;
+
+(* ------------------------------------------------------------------------------------
+ -- Fonction          : tirerPion(pioche: pointPioche; numeroCase: Integer): tPion
+ -- Auteur            : Guillaume Quentin
+ -- Date de creation  : 01 June 2018 (Friday) 16:03
+ --
+ -- But               : Tirer un pion aléatoir de la pioche et le retourner
+ -- Pré conditions    : Une pioche initialisée
+ -- Post conditions   : Tirer un pion aléatoir de la pioche et le retourner
+ ------------------------------------------------------------------------------------*)
+FUNCTION tirerPion(pioche: pointPioche; numeroCase: Integer): tPion;
+
+(* ------------------------------------------------------------------------------------
+ -- Fonction          : supprPioche(pioche: pointPioche; numeroCase: Integer): pioche
+ -- Auteur            : Guillaume Quentin
+ -- Date de creation  : 01 June 2018 (Friday) 16:45
+ --
+ -- But               : Supprimer une case d'une pioche
+ -- Pré conditions    : Une pioche non vide et un numéro de case
+ -- Post conditions   : Supprimer une case d'une pioche
+ ------------------------------------------------------------------------------------*)
+FUNCTION supprPioche(pioche: pointPioche; debutPioche: pointPioche; numeroCase: Integer): pointPioche;
 
 IMPLEMENTATION
 
@@ -82,6 +104,76 @@ BEGIN
     	res:=pioch;
     END;
     initPioche:= res;
+END;
+
+FUNCTION supprPioche(pioche: pointPioche; debutPioche: pointPioche; numeroCase: Integer): pointPioche;
+VAR
+    res, temp: pointPioche;
+BEGIN
+   IF(numeroCase <= pioche^.nombre) THEN
+   BEGIN
+        IF(pioche^.nombre = 1) THEN
+        BEGIN
+            res:= pioche^.next;
+            Dispose(pioche);
+        END
+        ELSE
+        BEGIN
+            pioche^.nombre:= pioche^.nombre - 1;
+            res:= pioche;
+        END;
+    END
+    ELSE
+    BEGIN
+        IF(pioche^.next = Nil) THEN
+        BEGIN
+            writeln('ERREUR, nombre incorrect');
+            res:= debutPioche;
+        END
+        ELSE
+        BEGIN
+            IF(numeroCase <= pioche^.next^.nombre + pioche^.nombre) THEN
+            BEGIN
+                IF(pioche^.next^.nombre = 1) THEN
+                BEGIN
+                    temp:= pioche^.next^.next;
+                    Dispose(pioche^.next);
+                    pioche^.next:= temp;
+                END
+                ELSE
+                BEGIN
+                    pioche^.next^.nombre:= pioche^.next^.nombre - 1;
+                END;
+                res:= debutPioche;
+            END
+            ELSE
+            BEGIN
+                res:= supprPioche(pioche^.next, debutPioche, numeroCase - pioche^.nombre);
+            END;
+       END;
+   END;
+   supprPioche:= res;
+END;
+
+///////////////////////////////////////////////////NON TESTE///////////////////////////////////////////////////////
+
+FUNCTION tirerPion(pioche: pointPioche; numeroCase: Integer): tPion;
+VAR
+    res: tPion;
+BEGIN
+	IF(numeroCase > pioche^.nombre) AND (pioche^.next<>Nil) THEN
+    BEGIN
+        res:= tirerPion(pioche^.next, numeroCase - pioche^.nombre);
+    END
+    ELSE IF ((pioche^.next=Nil) AND (numeroCase > pioche^.nombre)) OR (numeroCase <= 0) THEN
+    BEGIN
+        writeln('ERREUR: pioche erroné');
+    END
+    ELSE
+    BEGIN
+        res:= pioche^.pion;
+    END;
+    tirerPion:= res;
 END;
 
 END.
